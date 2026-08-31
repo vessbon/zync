3560bbd367cc31e81f5544963fcf19f36561131c
vessbon <vessbon@gmail.com>
2026-08-31 18:12:15 +0200

    fix(api): inject services when building app

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,11 +1,12 @@
 import { buildApp } from "./app";
 import env from "./env";
-import { authHandler, getSession, rootLogger } from "./lib/container";
+import { authHandler, getSession, rootLogger, services } from "./lib/container";
 
 const app = buildApp({
   rootLogger,
   getSession,
   authHandler,
+  services,
   isProduction: env.NODE_ENV === "production",
 });
 
