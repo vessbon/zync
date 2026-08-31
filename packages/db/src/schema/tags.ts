614fa86217c4d3dc5d7003be9199b8525bda0501
vessbon <vessbon@gmail.com>
2026-08-31 18:00:33 +0200

    feat(db): add index and unique constraints

--- packages/db/src/schema/tags.ts
+++ packages/db/src/schema/tags.ts
@@ -1,22 +1,38 @@
-import { integer, snakeCase, uuid, varchar } from "drizzle-orm/pg-core";
+import {
+  index,
+  integer,
+  snakeCase,
+  text,
+  unique,
+  uuid,
+  varchar,
+} from "drizzle-orm/pg-core";
 import { timestamps } from "../columns.helpers";
 import { users } from "./auth";
 
-export const tags = snakeCase.table("tags", {
-  id: uuid().defaultRandom().primaryKey(),
-  userId: uuid()
-    .references(() => users.id, {
-      onDelete: "cascade",
-    })
-    .notNull(),
-  name: varchar({ length: 20 }).notNull(),
-});
+export const tags = snakeCase.table(
+  "tags",
+  {
+    id: uuid().defaultRandom().primaryKey(),
+    userId: text()
+      .references(() => users.id, {
+        onDelete: "cascade",
+      })
+      .notNull(),
+    name: varchar({ length: 20 }).notNull(),
+  },
+  (table) => [unique().on(table.userId, table.name)],
+);
 
-export const time = snakeCase.table("time_entries", {
-  id: uuid().defaultRandom().primaryKey(),
-  tagId: uuid().references(() => tags.id, {
-    onDelete: "cascade",
-  }),
-  duration: integer(),
-  ...timestamps,
-});
+export const timeEntries = snakeCase.table(
+  "time_entries",
+  {
+    id: uuid().defaultRandom().primaryKey(),
+    tagId: uuid().references(() => tags.id, {
+      onDelete: "cascade",
+    }),
+    duration: integer(),
+    ...timestamps,
+  },
+  (table) => [index("time_entries_tag_id_idx").on(table.tagId)],
+);
