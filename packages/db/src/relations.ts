63375e92aafa879b077255a67fc0f287dae4cb2e
vessbon <vessbon@gmail.com>
2026-08-31 18:49:36 +0200

    fix(db): unused r type error

--- packages/db/src/relations.ts
+++ packages/db/src/relations.ts
@@ -1,4 +1,4 @@
 import { defineRelations } from "drizzle-orm";
 import * as schema from "./schema";
 
-export const relations = defineRelations(schema, (r) => ({}));
+export const relations = defineRelations(schema, (_) => ({}));
