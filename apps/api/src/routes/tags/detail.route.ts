c695db39d63bad122cc45c4a3d229985d8c173aa
vessbon <vessbon@gmail.com>
2026-09-01 18:27:07 +0200

    feat(api): add list tag route

---
+++ apps/api/src/routes/tags/detail.route.ts
@@ -0,0 +1,30 @@
+import { zValidator } from "@hono/zod-validator";
+import { ReasonPhrases, StatusCodes } from "http-status-codes";
+import * as z from "zod";
+import { createRouter } from "@/lib/create-app";
+
+const router = createRouter().get(
+  "/:id",
+  zValidator(
+    "param",
+    z.object({
+      id: z.uuid(),
+    }),
+  ),
+  async (c) => {
+    const { id } = c.req.valid("param");
+
+    const tag = await c.var.services.tagService.get(id);
+
+    if (!tag) {
+      return c.json(
+        { message: ReasonPhrases.NOT_FOUND },
+        StatusCodes.NOT_FOUND,
+      );
+    }
+
+    return c.json({ message: ReasonPhrases.OK, tag }, StatusCodes.OK);
+  },
+);
+
+export default router;
--- apps/api/src/routes/tags/index.ts
+++ apps/api/src/routes/tags/index.ts
@@ -1,8 +1,10 @@
 import { createRouter } from "@/lib/create-app";
 import create from "./create.route";
+import list from "./detail.route";
 
 const router = createRouter();
 
+router.route("/", list);
 router.route("/", create);
 
 export default router;
